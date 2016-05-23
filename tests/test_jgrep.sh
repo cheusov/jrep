d89164b04a9618e1a7ae506f0bf6bd5b41f9ff86
Aleksey Cheusov <vle@gmx.net>
2016-05-23 06:48:36 +0300

    Release 0.3

--- tests/test_jgrep.sh
+++ tests/test_jgrep.sh
@@ -310,12 +310,12 @@ $GREP_CMD -x --line-number 'This' text1.txt text2.txt |
 
 $GREP_CMD -V |
     cmp 'jgrep - #24.1' \
-'jgrep-0.2
+'jgrep-0.3
 '
 
 $GREP_CMD --version |
     cmp 'jgrep - #24.2' \
-'jgrep-0.2
+'jgrep-0.3
 '
 
 $GREP_CMD --line-buffered --with-filename OpenBSD text2.txt |

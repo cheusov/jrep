21167158d6067a3aaab4b0b5a803bbe8b2e3c4fa
Aleksey Cheusov <vle@gmx.net>
2016-05-26 21:19:19 +0300

    If JGREP_NO_JNI environment variable is set, jgrep_jni is not loaded

--- jar/src/main/java/com/cheusov/Utils.java
+++ jar/src/main/java/com/cheusov/Utils.java
@@ -3,23 +3,20 @@ package com.cheusov;
 import org.apache.commons.lang3.SystemUtils;
 
 public class Utils {
-    private static boolean osWindows = SystemUtils.IS_OS_WINDOWS;
+    private static boolean withJNI =
+            (System.getenv("JGREP_NO_JNI") == null) && !SystemUtils.IS_OS_WINDOWS;
 
     static {
-        if (! osWindows)
+        if (withJNI)
             System.loadLibrary("jgrep_jni");
     }
 
     private static native boolean isStdoutTTY();
 
-    private static boolean isStdoutTTY_Win() {
-        return false;
-    }
-
     public static boolean isStdoutTTY_Any() {
-        if (osWindows)
-            return isStdoutTTY_Win();
-        else
+        if (withJNI)
             return isStdoutTTY();
+        else
+            return true;
     }
 }
